55a075dee7a38cdd32067e92aa4c5e4b9c5fae53
muza97 <mustafa.zaitoun97@gmail.com>
2024-01-18 14:08:48 +0100

    connected to backend

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
-
-
 const express = require("express");
 const userRoutes = require ('./routes/userRoutes.js');
 const app = express();
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/users', userRoutes);
 app.get("/", (req, res) => {
    res.send("This is home page.");
